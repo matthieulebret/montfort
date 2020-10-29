77470663ba2cca6c81c9a920ed7cd06728dbd339
matthieulebret <73009187+matthieulebret@users.noreply.github.com>
2020-10-29 16:21:11 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ st.beta_set_page_config('Montfort immobilier',layout='wide')
 
 st.title('''Immobilier a Montfort l'Amaury''')
 
+st.header('Transactions realisees')
+
 ## Import data
 df1 = pd.read_csv(r'78420_0000F.csv',sep=';',thousands=',')
 df2 = pd.read_csv(r'78420_0000D.csv',sep=';',thousands=',')
@@ -50,12 +52,16 @@ df['prix_m2'] = df.apply(lambda x: priceperm2(x['type_local'],x['surface_reelle_
 
 df['Year']=pd.to_datetime(df['date_mutation']).dt.year
 
-with st.beta_expander('Voir les transactions realisees'):
+with st.beta_expander('Voir toutes les transactions realisees'):
     shortdf = df[['date_mutation','prix_m2','valeur_fonciere','type_local','surface_reelle_bati','surface_terrain','nombre_pieces_principales','nature_culture_speciale']]
     shortdf['valeur_fonciere'] = pd.to_numeric(shortdf['valeur_fonciere'])
     shortdf['surface_terrain'] = pd.to_numeric(shortdf['surface_terrain'])
     shortdf['surface_reelle_bati'] = pd.to_numeric(shortdf['surface_reelle_bati'])
     shortdf['nombre_pieces_principales'] = pd.to_numeric(shortdf['nombre_pieces_principales'])
+    st.table(shortdf.style.format({'valeur_fonciere':'{:,.2f}','surface_reelle_bati':'{:,.2f}','nombre_pieces_principales':'{:,.2f}','surface_terrain':'{:,.2f}','prix_m2':'{:,.2f}'}))
+
+with st.beta_expander('Voir toutes les transactions terrains seuls'):
+    shortdf = shortdf[shortdf['type_local']=='None']
     st.table(shortdf.style.format({'valeur_fonciere':'{:,.2f}','surface_reelle_bati':'{:,.2f}','nombre_pieces_principales':'{:,.2f}','surface_terrain':'{:,.2f}','prix_m2':'{:,.2f}'}))
 
 yearfilter = st.slider('Filtrer par periode',min_value=2015,max_value=2019,value=(2015,2019),step=1)
@@ -70,26 +76,27 @@ st.plotly_chart(fig,use_container_width=True)
 
 ## Distplot
 
-col1,col2 = st.beta_columns(2)
+st.subheader('Distribution des transactions realisees')
+# selectassets = st.multiselect('Choisissez le type de local',['Maison','None','Local industriel. commercial ou assimilé'],default=['Maison','None','Local industriel. commercial ou assimilé'])
+# df = df[df['type_local'].isin(selectassets)]
 
-with col1:
-    ##Sunburst charts
-    st.subheader('Transactions par type')
-    st.markdown('Cliquez sur le graphe pour obtenir plus de detail.')
-    fig = px.sunburst(df,path=['type_local','nature_culture_speciale','adresse_nom_voie','id_mutation'],values='valeur_fonciere')
-    st.plotly_chart(fig)
+highlight = alt.selection(type='interval',bind='scales',encodings=['x','y'])
 
-with col2:
-    st.subheader('Distribution des transactions')
-    selectassets = st.multiselect('Select type_local',['Maison','None','Local industriel. commercial ou assimilé'],default=['Maison','None','Local industriel. commercial ou assimilé'])
-    df = df[df['type_local'].isin(selectassets)]
-    fig = px.box(df,x='Year',y='valeur_fonciere',hover_data={'date_mutation':True,'adresse_nom_voie':True,'valeur_fonciere':':,.2f','type_local':True,'surface_reelle_bati':':,.2f','surface_terrain':':,.2f','prix_m2':':,.2f'},color='Year',points='all')
-    fig.update_layout(showlegend=False)
-    st.plotly_chart(fig)
+fig2 = alt.Chart(df).mark_circle(size=200).encode(alt.X('surface_terrain:Q',scale=alt.Scale(domain=[0,2000])),alt.Y('prix_m2:Q',scale=alt.Scale(domain=[-100,5000])),color='type_local:N',tooltip=[
+      {"type": "nominal", "field": "Year"},
+      {"type": "nominal", "field": "adresse_nom_voie"},
+      {"type": "quantitative", "field": "valeur_fonciere",'format':',.2f'},
+      {"type": "quantitative", "field": "surface_terrain",'format':',.2f'},
+      {"type": "quantitative", "field": "prix_m2",'format':',.2f'}]).add_selection(highlight).configure_legend(orient='bottom')
+st.altair_chart(fig2,use_container_width=True)
+# fig = px.box(df,x='Year',y='valeur_fonciere',hover_data={'date_mutation':True,'adresse_nom_voie':True,'valeur_fonciere':':,.2f','type_local':True,'surface_reelle_bati':':,.2f','surface_terrain':':,.2f','prix_m2':':,.2f'},color='Year',points='all')
+# fig.update_layout(showlegend=False)
+# st.plotly_chart(fig)
 
 ### WEB SCRAPING
 
-st.header('Dernieres annonces')
+st.header('Dernieres annonces de terrains nus')
+st.markdown('Source: https://www.terrain-construction.com')
 
 location = []
 postcode = []
@@ -137,5 +144,5 @@ fig = alt.Chart(mylist).mark_circle(size=200).encode(alt.X('Surface:Q',scale=alt
       {"type": "nominal", "field": "Commune"},
       {"type": "quantitative", "field": "Surface",'format':',.2f'},
       {"type": "quantitative", "field": "Prix",'format':',.2f'},
-      {"type": "quantitative", "field": "Prix_m2",'format':'.2f'}]).add_selection(highlight)
+      {"type": "quantitative", "field": "Prix_m2",'format':'.2f'}]).add_selection(highlight).configure_legend(orient='bottom')
 st.altair_chart(fig,use_container_width=True)
